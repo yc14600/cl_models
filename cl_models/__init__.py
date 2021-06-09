fa85c8d04f275568c5ee93dbbcc963db627a077d
Yu Chen <yc14600@bristol.ac.uk>
2021-06-09 21:00:37 +0100

    update

--- cl_models/__init__.py
+++ cl_models/__init__.py
@@ -2,4 +2,3 @@ from .generative.cvae import Continual_VAE, Continual_DVAE
 from .generative.ccgan import Continual_CGAN, Continual_Ratio_fGAN
 from .discriminative.vcl.models.vcl_model import VCL
 from .discriminative.vcl.models.bcl_base_bnn import BCL_BNN
-from .discriminative.ewc.EWC_Model import EWC_Model
